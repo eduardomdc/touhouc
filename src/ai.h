898c16238687cd5428a73c708c1d0fe64cb49b38
eduardomdc <eduardomc@dcc.ufrj.br>
2025-02-25 11:32:15 -0300

    Added statemachine groundwork

---
+++ src/ai.h
@@ -0,0 +1,24 @@
+#ifndef AI_H
+#define AI_H
+#include <raylib.h>
+#include "timer.h"
+
+typedef enum {
+    ANGEL_STATE_INITIAL,
+    ANGEL_STATE_MOVE,
+    ANGEL_STATE_FIRE,
+    ANGEL_STATE_LEN
+} AngelStates;
+
+void angelStateInitial(void* enemy);
+void angelStateMove(void* enemy);
+void angelStateFire(void* enemy);
+extern void (*angelStateUpdate[ANGEL_STATE_LEN])(void*);
+
+typedef struct {
+    char state;
+    Vector2 targetPos;
+    Timer stateTimer;
+} StateMachine;
+
+#endif
